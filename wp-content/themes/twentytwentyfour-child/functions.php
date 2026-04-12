6f73a2319652b593ac6315432b9ec142ccc7e8c9
kphjelmer <kp@usefulinnovation.se>
2026-04-12 15:47:35 +0200

    Lägg till GTM-5JNVFKDV på alla sidor

--- wp-content/themes/twentytwentyfour-child/functions.php
+++ wp-content/themes/twentytwentyfour-child/functions.php
@@ -7,6 +7,34 @@
  */
 
 /**
+ * Google Tag Manager – GTM-5JNVFKDV
+ * Snippet i <head> (måste vara så tidigt som möjligt)
+ */
+add_action('wp_head', function () {
+    ?>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5JNVFKDV');</script>
+<!-- End Google Tag Manager -->
+    <?php
+}, 1); // Prio 1 = laddas före allt annat i <head>
+
+/**
+ * Google Tag Manager – noscript-fallback direkt efter <body>
+ */
+add_action('wp_body_open', function () {
+    ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JNVFKDV"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+    <?php
+}, 1);
+
+/**
  * Ladda styles korrekt (parent först, child ovanpå).
  */
 add_action('wp_enqueue_scripts', function () {
